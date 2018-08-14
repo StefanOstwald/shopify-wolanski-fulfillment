ae276ebd2c6be3f2cfbcc6787413a7ecf8945375
Stefan Ostwald <stefan@futureofvoice.com>
2018-08-14 21:20:19 +0200

    applied Wolanski requirement:no quotes in csv
    added security: removes all occurances of the delimiter (;) in a string so the system can handle user input which contains the delimiter.

--- src/wolanski/fileCreater.test.js
+++ src/wolanski/fileCreater.test.js
@@ -7,4 +7,72 @@ describe('fileCreater', () => {
     console.log(`FileCreater.genFileName(): ${JSON.stringify(FileCreator.genFileName(), null, 2)}`);
     assert.notEqual(FileCreator.genFileName(), '1970-01-01-T-01-00-00.csv');
   });
+
+  describe('replaceDelimiterInAllStringsWithReplacer', () => {
+    const delimiter = ';';
+    const replacer = '-';
+
+    it('doesnt touch dates', () => {
+      const input = {
+        dat: new Date(0),
+      };
+      assert.deepEqual(FileCreator.replaceDelimiterInAllStringsWithReplacer(input, delimiter, replacer), input);
+    });
+
+    it('replaces nested objects', () => {
+      const input = {
+        a: {
+          b: {
+            c: 'aaa;aa',
+            in: 123,
+          },
+        },
+      };
+      const output = {
+        a: {
+          b: {
+            c: 'aaa-aa',
+            in: 123,
+          },
+        },
+      };
+      assert.deepEqual(FileCreator.replaceDelimiterInAllStringsWithReplacer(input, delimiter, replacer), output);
+    });
+
+    it('replaces objects in arrays', () => {
+      const input = {
+        a: {
+          arr: [{
+            item: {
+              str: 'arr;a',
+            },
+          }],
+        },
+      };
+      const output = {
+        a: {
+          arr: [{
+            item: {
+              str: 'arr-a',
+            },
+          }],
+        },
+      };
+      assert.deepEqual(FileCreator.replaceDelimiterInAllStringsWithReplacer(input, delimiter, replacer), output);
+    });
+
+    it('replaces objects string arrays', () => {
+      const input = {
+        a: {
+          arr: ['arr;a'],
+        },
+      };
+      const output = {
+        a: {
+          arr: ['arr-a'],
+        },
+      };
+      assert.deepEqual(FileCreator.replaceDelimiterInAllStringsWithReplacer(input, delimiter, replacer), output);
+    });
+  });
 });
--- src/wolanski/fileCreator.js
+++ src/wolanski/fileCreator.js
@@ -6,12 +6,31 @@ import { TimeKeeper } from '../util/timeKeeper';
 
 export class FileCreator {
   static createCsvFileFromArray(csvArray) {
+    const delimiter = ';';
     const fields = _.keys(getEmptyOrder());
-    const file = json2csv({ data: csvArray, fields, del: ';' });
+    const file = json2csv({
+      data: csvArray, fields, del: delimiter, quotes: '',
+    });
     return file;
   }
 
   static genFileName() {
     return `${TimeKeeper.getLocalTime().format('YYYY-MM-DD-T-HH-mm-ss')}.csv`;
+  }
+
+  static replaceDelimiterInAllStringsWithReplacer(obj, delimiter = ';', replacer = ' ') {
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === 'string') {
+        obj[key] = obj[key].replace(delimiter, replacer);
+        return;
+      }
+
+      const valueContainsSubattributes = typeof obj[key] === 'object' && Object.keys(obj[key]).length > 0;
+      if (valueContainsSubattributes) {
+        obj[key] = FileCreator.replaceDelimiterInAllStringsWithReplacer(obj[key], delimiter, replacer);
+      }
+    });
+
+    return obj;
   }
 }
