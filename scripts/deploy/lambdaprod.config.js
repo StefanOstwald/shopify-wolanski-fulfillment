b7b01a5dc313c9b27e93836ff851bf3f06914f79
Stefan Ostwald <stefan@futureofvoice.com>
2020-04-11 23:33:24 +0200

    updated deploy script to use nodejs12.x

--- scripts/deploy/lambdaprod.config.js
+++ scripts/deploy/lambdaprod.config.js
@@ -9,6 +9,7 @@ const { description, version } = require('../../package.json');
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const region = process.env.AWS_DEFAULT_REGION;
+const runtime = "nodejs12.x";
 
 export default {
   accessKeyId,
@@ -16,5 +17,6 @@ export default {
   functionName: lambdaFunctionName,
   description: `${description} (version ${version})`,
   region,
+  runtime,
   handler: 'index.handler',
 };
